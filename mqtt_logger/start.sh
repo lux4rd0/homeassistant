c871ee3e403743ff52d22de91e5f5c961cb572fc
Dave Schmid <dave@pulpfree.org>
2022-06-25 14:39:01 -0500

    Delete mqtt_logger directory

--- mqtt_logger/start.sh
+++
@@ -1,2 +0,0 @@
-mosquitto_sub -h $MQTT_LOGGER_MQTT_HOSTNAME -p $MQTT_LOGGER_MQTT_PORT -u $MQTT_LOGGER_MQTT_USERNAME -P $MQTT_LOGGER_MQTT_PASSWORD -F %j -t '+/debug' -t 'homeassistant/#' | promtail --stdin --config.expand-env=true --config.file=/mqtt_logger/config.yaml
-
