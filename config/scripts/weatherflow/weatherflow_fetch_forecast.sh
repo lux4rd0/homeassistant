2d9ffeee4c633bd73d4f48d61dc899ad1e755246
Dave Schmid <dave@pulpfree.org>
2022-06-25 22:23:35 -0500

    Update weatherflow_fetch_forecast.sh

--- config/scripts/weatherflow/weatherflow_fetch_forecast.sh
+++ config/scripts/weatherflow/weatherflow_fetch_forecast.sh
@@ -3,13 +3,13 @@
 WEATHERFLOW_STATION_ID="<station_id>"
 WEATEHRFLOW_TOKEN="<token>"
 
-WEATEHRFLOW_UNITS_TEMP="f"
-WEATEHRFLOW_UNITS_WIND="mph"
-WEATEHRFLOW_UNITS_PRESSURE="mmhg"
-WEATEHRFLOW_UNITS_PRECIP="in"
-WEATEHRFLOW_UNITS_DISTANCE="mi"
+WEATHERFLOW_UNITS_TEMP="f"
+WEATHERFLOW_UNITS_WIND="mph"
+WEATHERFLOW_UNITS_PRESSURE="mmhg"
+WEATHERFLOW_UNITS_PRECIP="in"
+WEATHERFLOW_UNITS_DISTANCE="mi"
 
-URL="https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${WEATHERFLOW_STATION_ID}&units_temp=${WEATEHRFLOW_UNITS_TEMP}&units_wind=${WEATEHRFLOW_UNITS_WIND}&units_pressure=${WEATEHRFLOW_UNITS_PRESSURE}&units_precip=${WEATEHRFLOW_UNITS_PRECIP}&units_distance=${WEATEHRFLOW_UNITS_DISTANCE}&token=${WEATEHRFLOW_TOKEN}"
+URL="https://swd.weatherflow.com/swd/rest/better_forecast?station_id=${WEATHERFLOW_STATION_ID}&units_temp=${WEATHERFLOW_UNITS_TEMP}&units_wind=${WEATHERFLOW_UNITS_WIND}&units_pressure=${WEATHERFLOW_UNITS_PRESSURE}&units_precip=${WEATHERFLOW_UNITS_PRECIP}&units_distance=${WEATHERFLOW_UNITS_DISTANCE}&token=${WEATHERFLOW_TOKEN}"
 
 curl -s -X GET --header 'Accept: application/json' "${URL}" -o /config/scripts/weatherflow/weatherflow_forecast_out.txt.tmp
 mv /config/scripts/weatherflow/weatherflow_forecast_out.txt.tmp /config/scripts/weatherflow/weatherflow_forecast_out.txt
