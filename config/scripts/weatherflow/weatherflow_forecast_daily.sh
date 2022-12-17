efe454441288031a1fdaf21a14c9cdd079090093
Dave Schmid <dave@pulpfree.org>
2022-12-17 11:13:06 -0600

    Fixed sleet icon and BC negative temperature calculation

--- config/scripts/weatherflow/weatherflow_forecast_daily.sh
+++ config/scripts/weatherflow/weatherflow_forecast_daily.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 output=$(</config/scripts/weatherflow/weatherflow_forecast_out.txt)
+#output=$(<weatherflow_forecast_out.txt)
 
 daily_day_name=$(echo "${output}" | jq -r '.forecast.daily['"$1"'].day_start_local | strflocaltime("%A") | ascii_upcase')
 
@@ -9,6 +10,13 @@ daily_month_num=$(echo "${output}" | jq -r '.forecast.daily['"$1"'].month_num')
 
 daily_air_temp_high=$(echo "${output}" | jq -r '.forecast.daily['"$1"'].air_temp_high')
 daily_air_temp_low=$(echo "${output}" | jq -r '.forecast.daily['"$1"'].air_temp_low')
+
+forecast_temperature_high=$(echo "${output}" | jq .forecast.daily[0:9] |jq ' [ .[].air_temp_high ] | max ')
+forecast_temperature_low=$(echo "${output}" | jq .forecast.daily[0:9] |jq ' [ .[].air_temp_low ] | min ')
+forecast_temperature_difference=$(bc <<< "${forecast_temperature_high} - ${forecast_temperature_low}")
+
+air_temp_low_x=$(bc <<< "scale=2;((${daily_air_temp_low} - ${forecast_temperature_low})/${forecast_temperature_difference})*100")
+air_temp_high_x=$(bc <<< "scale=2;100-((${forecast_temperature_high} - ${daily_air_temp_high})/${forecast_temperature_difference})*100")
 
 daily_conditions=$(echo "${output}" | jq -r '.forecast.daily['"$1"'].conditions | ascii_upcase')
 
@@ -33,7 +41,7 @@ if [ "${daily_icon}" == "possibly-snow-night" ]; then daily_icon="O"; fi
 if [ "${daily_icon}" == "possibly-thunderstorm-day" ]; then daily_icon="y"; fi
 if [ "${daily_icon}" == "possibly-thunderstorm-night" ]; then daily_icon="x"; fi
 if [ "${daily_icon}" == "rainy" ]; then daily_icon="b"; fi
-if [ "${daily_icon}" == "sleet" ]; then daily_icon="%%"; fi
+if [ "${daily_icon}" == "sleet" ]; then daily_icon="%"; fi
 if [ "${daily_icon}" == "snow" ]; then daily_icon="."; fi
 if [ "${daily_icon}" == "thunderstorm" ]; then daily_icon="w"; fi
 if [ "${daily_icon}" == "windy" ]; then daily_icon="j"; fi
@@ -43,7 +51,9 @@ echo "{
   \"icon\": \"${daily_icon}\",
   \"day_name\": \"${daily_day_name}\",
   \"air_temp_high\": ${daily_air_temp_high},
+  \"air_temp_high_x\": ${air_temp_high_x},
   \"air_temp_low\": ${daily_air_temp_low},
+  \"air_temp_low_x\": ${air_temp_low_x},
   \"precip_probability\": ${daily_precip_probability},
   \"day_num\": ${daily_day_num},
   \"month_num\": ${daily_month_num},
